f384010d59200cd63d04a6a1c65a9bd94f027603
sharlaynejardini <sharlayne.angel.jardini@gmail.com>
2026-02-25 09:34:36 -0300

    atualização vercel banco

--- src/api.js
+++ src/api.js
@@ -7,7 +7,7 @@ import axios from "axios";
 
 // Ajuste essa URL quando fizer deploy
 const api = axios.create({
-  baseURL: "https://back-conteudo-takaoka.onrender.com/"
+  baseURL: "https://novo-serveless-conteudo.vercel.app/"
 });
 
 export default api;
