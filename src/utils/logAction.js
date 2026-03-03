30caea80724057f3f9a99ac5362f0932ab976323
sharlaynejardini <sharlayne.angel.jardini@gmail.com>
2026-03-03 11:03:53 -0300

    painel admin5

--- src/utils/logAction.js
+++ src/utils/logAction.js
@@ -2,17 +2,25 @@ import { supabase } from "../supabaseClient";
 
 export async function logAction(action) {
 
+  console.log("LOG ACTION CHAMADO:", action); // 👈 ADICIONE ESTA LINHA
+
   const { data } = await supabase.auth.getSession();
 
   if (!data.session) return;
 
   const user = data.session.user;
 
-  await supabase.from("action_logs").insert([
-    {
-      user_id: user.id,
-      email: user.email,
-      action: action
-    }
-  ]);
+  const { error } = await supabase
+    .from("action_logs")
+    .insert([
+      {
+        user_id: user.id,
+        email: user.email,
+        action: action
+      }
+    ]);
+
+  if (error) {
+    console.error("Erro ao registrar ação:", error);
+  }
 }
