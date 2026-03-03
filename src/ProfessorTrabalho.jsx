2991f3ff22269d19e54f8f98569fab7640d532b3
sharlaynejardini <sharlayne.angel.jardini@gmail.com>
2026-03-03 10:57:28 -0300

    painel admin4

--- src/ProfessorTrabalho.jsx
+++ src/ProfessorTrabalho.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import api from "./api";
-import { logAction } from "./logAction";
+import { logAction } from "./utils/logAction";
 
 function ProfessorTrabalho() {
 
